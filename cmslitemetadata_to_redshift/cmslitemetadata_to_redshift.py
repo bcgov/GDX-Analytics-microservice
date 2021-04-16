6c56fecf6f003f3583dc56d8cf6a8e8a4c757907
bjenning <brendan.jennings@gov.bc.ca>
2021-04-16 10:20:03 -0700

    Fixed folder_name logic.

--- cmslitemetadata_to_redshift/cmslitemetadata_to_redshift.py
+++ cmslitemetadata_to_redshift/cmslitemetadata_to_redshift.py
@@ -761,7 +761,7 @@ UPDATE {dbschema}.metadata
     SET folder_name = l2.title 
     FROM {dbschema}.metadata as l1 
     INNER JOIN {dbschema}.metadata as l2 ON l1.parent_node_id = l2.node_id 
-    WHERE l1.page_type = 'ASSET';
+    WHERE l1.parent_node_id in (select node_id from cmslite.metadata where page_type like 'ASSET_FOLDER'));
 COMMIT;
     """.format(dbschema=dbschema)
 
