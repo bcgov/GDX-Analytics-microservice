bab6fcb17fb6df4252e81718434aa54b906b599a
Lane Clark <52838144+lclarko@users.noreply.github.com>
2021-07-12 12:31:32 -0700

    Update google.google_sites.sql

--- google-api/google.google_sites.sql
+++ google-api/google.google_sites.sql
@@ -1,7 +1,9 @@
 CREATE TABLE google.google_sites (
   ref_site VARCHAR(255) ENCODE ZSTD, 
-  sc_domain boolean ENCODE ZSTD, 
-  overlap boolean ENCODE ZSTD, 
-  start_date date ENCODE AZ64, 
+  sc_domain BOOLEAN ENCODE ZSTD, 
+  overlap BOOLEAN ENCODE ZSTD, 
+  start_date DATE ENCODE AZ64, 
   sc_urlhost VARCHAR(255) ENCODE ZSTD
 );
+ALTER TABLE google.google_sites OWNER TO microservice;
+GRANT SELECT ON TABLE google.google_sites TO LOOKER; 
