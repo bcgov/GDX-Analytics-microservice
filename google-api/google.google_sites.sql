760daa3ea81868d20db83735cb8c44898ad7dd0c
Lane Clark <52838144+lclarko@users.noreply.github.com>
2021-10-06 20:27:09 -0700

    Update google.google_sites.sql

--- google-api/google.google_sites.sql
+++ google-api/google.google_sites.sql
@@ -1,7 +1,6 @@
 CREATE TABLE google.google_sites (
   ref_site VARCHAR(255) ENCODE ZSTD, 
   sc_domain BOOLEAN ENCODE ZSTD, 
-  overlap BOOLEAN ENCODE ZSTD, 
   start_date DATE ENCODE AZ64, 
   sc_urlhost VARCHAR(255) ENCODE ZSTD
 );
