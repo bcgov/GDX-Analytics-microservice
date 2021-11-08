bd5e8fb952467bb64f6eae38c70ec6e84f2daeae
Mark Walle <mark.walle@gov.bc.ca>
2021-11-08 13:26:23 -0800

    changes to pmrp qdata to provide service_creation_duration_per_visit column, with config for historical data unload

--- sfts/dml/pmrp_qdata_range.sql
+++ sfts/dml/pmrp_qdata_range.sql
@@ -35,12 +35,13 @@ SELECT * FROM(
     (1.00 * cfms_poc.waiting_duration_total)/(60*60*24)  AS "cfms_poc.waiting_duration_per_visit",
     (1.00 * cfms_poc.hold_duration)/(60*60*24)  AS "cfms_poc.hold_duration_per_service",
     (1.00 * cfms_poc.hold_duration_total)/(60*60*24)  AS "cfms_poc.hold_duration_per_visit",
+    (1.00 * cfms_poc.service_creation_duration)/(60*60*24)  AS "cfms_poc.service_creation_duration_per_visit",
     COUNT (DISTINCT cfms_poc.client_id )  AS "cfms_poc.visits_count",
     COUNT(*) AS "cfms_poc.services_count",
     COALESCE(SUM(cfms_poc.transaction_count ), 0) AS "cfms_poc.transactions_count"
     FROM derived.theq_step1  AS cfms_poc
     WHERE cfms_poc.welcome_time BETWEEN {pmrp_date_range}
     AND (((TRANSLATE(TRANSLATE(cfms_poc.office_name, '' '', ''_''),''.'','''')) LIKE ''%''))
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29
     ORDER BY 14
 )
