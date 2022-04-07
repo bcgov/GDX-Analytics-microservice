542d07ffadcc1f972fc5c86792ce47362af3e567
Dan Pollock <dan.pollock@gov.bc.ca>
2022-04-07 22:24:57 +0000

    updates to chatbot config

--- s3_to_redshift/ddl/microservice.chatbot_reports.sql
+++ s3_to_redshift/ddl/microservice.chatbot_reports.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS microservice.chatbot_reports
  (
+    "dashboard_id" VARCHAR(63) ENCODE ZSTD,
     "conf_score" VARCHAR(63) ENCODE ZSTD,
     "sentiment_score" DECIMAL(3,2) ENCODE ZSTD,
     "sentiment_magnitude" DECIMAL(3,2) ENCODE ZSTD,
-    "sessionorigin" VARCHAR(255) ENCODE ZSTD,
-    "conversation_index" DECIMAL ENCODE ZSTD,
     "conversation_length" DECIMAL ENCODE ZSTD,
     "conversation_duration" DECIMAL ENCODE ZSTD,
+    "sessionorigin" VARCHAR(255) ENCODE ZSTD,
     "session_id" VARCHAR(255) ENCODE ZSTD,
     "timestamp" VARCHAR(255) ENCODE ZSTD,
     "matched_intent" VARCHAR(255) ENCODE ZSTD,
