9b6fc663f8c399273589fd89f94b8cc317df4635
doughon <76968796+doughon@users.noreply.github.com>
2022-11-16 11:35:44 -0800

    Updated DMLs to use stable name PDTs

--- sfts/dml/sbc_oa_click_last_full_day.sql
+++ sfts/dml/sbc_oa_click_last_full_day.sql
@@ -1,42 +1,24 @@
-SELECT * FROM
-(
+SELECT * FROM (
+  -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments_clicks?qid=8daE9wqfF3gUfy8RwmZHbm&origin_space=37&toggle=fil,pik,vis
+  -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments_clicks
   SELECT
-  ac.root_id, wp.id AS page_view_id,
-  pv.session_id,
-  label,
-  logged_in,
-  appointment_step,
-  location,
-  service,
-  url,
-  CASE WHEN label = ''Login: BCeID'' THEN 1 ELSE 0 END AS bceid_login_count,
-  CASE WHEN label = ''Create: BCeID'' THEN 1 ELSE 0 END AS bceid_create_count,
-  CASE WHEN label = ''Register'' THEN 1 ELSE 0 END AS register_count,
-  CASE WHEN label = ''Info: Privacy Statement'' THEN 1 ELSE 0 END AS privacy_count,
-  CASE WHEN label = ''Help'' THEN 1 ELSE 0 END AS help_count,
-  CASE WHEN label = ''Online Option'' THEN 1 ELSE 0 END AS online_option_count,
-  CASE WHEN label = ''Login'' THEN 1 ELSE 0 END AS login_count,
-  CASE WHEN label = ''View Location Services'' THEN 1 ELSE 0 END AS location_services_count,
-  CASE WHEN label = ''Login: BC Services Card'' THEN 1 ELSE 0 END AS bcsc_login_count,
-  CASE WHEN label = ''Info: About the BCeID'' THEN 1 ELSE 0 END AS bceid_info_count,
-  CASE WHEN label NOT IN (
-    ''Login: BCeID'',
-    ''Create: BCeID'',
-    ''Register'',
-    ''Info: Privacy Statement'',
-    ''Help'',
-    ''Online Option'',
-    ''Login'',
-    ''View Location Services'',
-    ''Login: BC Services Card'',
-    ''Info: About the BCeID''
-    ) THEN 1 ELSE 0 END AS other_count,
-  CONVERT_TIMEZONE(''UTC'', ''America/Vancouver'', ac.root_tstamp) AS timestamp
-FROM atomic.ca_bc_gov_cfmspoc_appointment_click_1 AS ac
-  JOIN atomic.com_snowplowanalytics_snowplow_web_page_1 AS wp
-      ON ac.root_id = wp.root_id AND ac.root_tstamp = wp.root_tstamp
-  JOIN derived.page_views AS pv ON pv.page_view_id = wp.id
-WHERE
-  timestamp > DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) AND
-  timestamp < DATE_TRUNC(''day'',GETDATE())
+      sbc_online_appointments_clicks.timestamp AS "sbc_online_appointments_clicks.timestamp",
+      sbc_online_appointments_clicks.session_id AS "sbc_online_appointments_clicks.session_id",
+      sbc_online_appointments_clicks.service AS "sbc_online_appointments_clicks.service",
+      sbc_online_appointments_clicks.url AS "sbc_online_appointments_clicks.url",
+      sbc_online_appointments_clicks.appointment_step AS "sbc_online_appointments_clicks.appointment_step",
+      sbc_online_appointments_clicks.step_order AS "sbc_online_appointments_clicks.step_order",
+      COUNT(DISTINCT sbc_online_appointments_clicks.session_id) AS "sbc_online_appointments_clicks.session_count"
+  FROM looker.BM_snowplow_web_block_sbc_online_appointments_clicks AS sbc_online_appointments_clicks
+  LEFT JOIN derived.page_views  AS page_views ON page_views.page_view_id = sbc_online_appointments_clicks.page_view_id
+  WHERE ((( sbc_online_appointments_clicks.timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments_clicks.timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) ))))) AND (sbc_online_appointments_clicks.label) = ''Online Option'' AND (page_views.page_urlhost ) IS NOT NULL
+  GROUP BY
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+  ORDER BY
+      1
 )
--- sfts/dml/sbc_oa_final_step_of_session_last_full_day.sql
+++ sfts/dml/sbc_oa_final_step_of_session_last_full_day.sql
@@ -1,55 +1,15 @@
-SELECT * FROM
-(
-  WITH raw_list AS (
-  SELECT ap.root_id, wp.id AS page_view_id,
-    logged_in,
-    status,
-    appointment_id,
-    appointment_step,
-    client_id,
-    location,
-    service,
-    pv.session_id,
-    CASE WHEN appointment_step = ''Location Selection'' THEN 1
-      WHEN appointment_step = ''Select Service'' THEN 2
-      WHEN appointment_step = ''Select Date'' THEN 3
-      WHEN appointment_step = ''Login to Confirm Appointment'' THEN 4
-      WHEN appointment_step = ''Appointment Summary'' THEN 5
-      WHEN appointment_step = ''Appointment Confirmed'' THEN 6
-      ELSE NULL END AS step_order,
-    CONVERT_TIMEZONE(''UTC'', ''America/Vancouver'', ap.root_tstamp) AS timestamp
-  FROM atomic.ca_bc_gov_cfmspoc_appointment_step_1 AS ap
-    JOIN atomic.com_snowplowanalytics_snowplow_web_page_1 AS wp
-      ON ap.root_id = wp.root_id AND ap.root_tstamp = wp.root_tstamp
-    JOIN derived.page_views AS pv ON pv.page_view_id = wp.id
-  WHERE status = ''new'' -- for now, report only on new appointments, not updates
-    AND timestamp > DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) )
-    AND timestamp < DATE_TRUNC(''day'',GETDATE())
-  ),
-  final_selections AS ( -- this is to assign the location and service as the latest selection
-    SELECT
-      session_id,
-      location,
-      service,
-      appointment_step,
-      appointment_id,
-      client_id,
-      ROW_NUMBER() OVER (PARTITION BY session_id ORDER BY step_order DESC, timestamp DESC) AS session_id_ranked
-    FROM raw_list
-  )
+SELECT * FROM (
+  -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments?qid=CUcFP7cBkxnOBdqLolIG2H&origin_space=37&toggle=fil,pik,vis
+  -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments
   SELECT
-    rl.session_id,
-    fs.location,
-    fs.service,
-    fs.appointment_step,
-    fs.appointment_id,
-    fs.client_id,
-    MIN(timestamp) AS min_timestamp,
-    MAX(timestamp) AS max_timestamp,
-    MAX(step_order) AS latest_step
-  FROM raw_list AS rl
-  JOIN final_selections AS fs
-    ON fs.session_id = rl.session_id
-    AND fs.session_id_ranked = 1
-  GROUP BY 1,2,3,4,5,6
+      sbc_online_appointments.min_timestamp AS "sbc_online_appointments.min_timestamp",
+      sbc_online_appointments.max_timestamp AS "sbc_online_appointments.max_timestamp",
+      sbc_online_appointments.session_id AS "sbc_online_appointments.session_id",
+      sbc_online_appointments.appointment_step AS "sbc_online_appointments.appointment_step",
+      sbc_online_appointments.client_id AS "sbc_online_appointments.client_id",
+      sbc_online_appointments.appointment_id AS "sbc_online_appointments.appointment_id"
+  FROM looker.BM_snowplow_web_block_sbc_online_appointments AS sbc_online_appointments
+  WHERE ((( sbc_online_appointments.min_timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments.min_timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) )))))
+  ORDER BY
+      1
 )
