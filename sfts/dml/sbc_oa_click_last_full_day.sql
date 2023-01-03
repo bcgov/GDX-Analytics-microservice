ba5686a242e585091a37ebc85576403479ffb00a
doughon <76968796+doughon@users.noreply.github.com>
2023-01-03 14:15:32 -0800

    Updated DML files to use correct datetime format

--- sfts/dml/sbc_oa_click_last_full_day.sql
+++ sfts/dml/sbc_oa_click_last_full_day.sql
@@ -1,24 +1,24 @@
 SELECT * FROM (
-  -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments_clicks?qid=8daE9wqfF3gUfy8RwmZHbm&origin_space=37&toggle=fil,pik,vis
+  -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments_clicks?qid=4Aj5Ua6FHKbjY7ognYW2aP&origin_space=37&toggle=fil,vis
   -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments_clicks
-  SELECT
-      sbc_online_appointments_clicks.timestamp AS "sbc_online_appointments_clicks.timestamp",
-      sbc_online_appointments_clicks.session_id AS "sbc_online_appointments_clicks.session_id",
-      sbc_online_appointments_clicks.service AS "sbc_online_appointments_clicks.service",
-      sbc_online_appointments_clicks.url AS "sbc_online_appointments_clicks.url",
-      sbc_online_appointments_clicks.appointment_step AS "sbc_online_appointments_clicks.appointment_step",
-      sbc_online_appointments_clicks.step_order AS "sbc_online_appointments_clicks.step_order",
-      COUNT(DISTINCT sbc_online_appointments_clicks.session_id) AS "sbc_online_appointments_clicks.session_count"
-  FROM looker.BM_snowplow_web_block_sbc_online_appointments_clicks AS sbc_online_appointments_clicks
-  LEFT JOIN derived.page_views  AS page_views ON page_views.page_view_id = sbc_online_appointments_clicks.page_view_id
-  WHERE ((( sbc_online_appointments_clicks.timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments_clicks.timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) ))))) AND (sbc_online_appointments_clicks.label) = ''Online Option'' AND (page_views.page_urlhost ) IS NOT NULL
-  GROUP BY
-      1,
-      2,
-      3,
-      4,
-      5,
-      6
-  ORDER BY
-      1
+    SELECT
+        (TO_CHAR(DATE_TRUNC(''second'', sbc_online_appointments_clicks.timestamp ), ''YYYY-MM-DD HH24:MI:SS'')) AS "sbc_online_appointments_clicks.click_time",
+        sbc_online_appointments_clicks.session_id AS "sbc_online_appointments_clicks.session_id",
+        sbc_online_appointments_clicks.service AS "sbc_online_appointments_clicks.service",
+        sbc_online_appointments_clicks.url AS "sbc_online_appointments_clicks.url",
+        sbc_online_appointments_clicks.appointment_step AS "sbc_online_appointments_clicks.appointment_step",
+        sbc_online_appointments_clicks.step_order AS "sbc_online_appointments_clicks.step_order",
+        COUNT(DISTINCT sbc_online_appointments_clicks.session_id) AS "sbc_online_appointments_clicks.session_count"
+    FROM looker.BM_snowplow_web_block_sbc_online_appointments_clicks AS sbc_online_appointments_clicks
+    LEFT JOIN derived.page_views  AS page_views ON page_views.page_view_id = sbc_online_appointments_clicks.page_view_id
+    WHERE ((( sbc_online_appointments_clicks.timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments_clicks.timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) ))))) AND (sbc_online_appointments_clicks.label) = ''Online Option'' AND (page_views.page_urlhost ) IS NOT NULL
+    GROUP BY
+        1,
+        2,
+        3,
+        4,
+        5,
+        6
+    ORDER BY
+        1 DESC
 )
--- sfts/dml/sbc_oa_final_step_of_session_last_full_day.sql
+++ sfts/dml/sbc_oa_final_step_of_session_last_full_day.sql
@@ -1,15 +1,22 @@
 SELECT * FROM (
-  -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments?qid=CUcFP7cBkxnOBdqLolIG2H&origin_space=37&toggle=fil,pik,vis
-  -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments
-  SELECT
-      sbc_online_appointments.min_timestamp AS "sbc_online_appointments.min_timestamp",
-      sbc_online_appointments.max_timestamp AS "sbc_online_appointments.max_timestamp",
-      sbc_online_appointments.session_id AS "sbc_online_appointments.session_id",
-      sbc_online_appointments.appointment_step AS "sbc_online_appointments.appointment_step",
-      sbc_online_appointments.client_id AS "sbc_online_appointments.client_id",
-      sbc_online_appointments.appointment_id AS "sbc_online_appointments.appointment_id"
-  FROM looker.BM_snowplow_web_block_sbc_online_appointments AS sbc_online_appointments
-  WHERE ((( sbc_online_appointments.min_timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments.min_timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) )))))
-  ORDER BY
-      1
+    -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments?qid=XWockxwvaClhUQh0PaPrXH&origin_space=37&toggle=fil,vis
+    -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments
+    SELECT
+        (TO_CHAR(DATE_TRUNC(''second'', sbc_online_appointments.min_timestamp ), ''YYYY-MM-DD HH24:MI:SS'')) AS "sbc_online_appointments.min_time_time",
+            (TO_CHAR(DATE_TRUNC(''second'', sbc_online_appointments.max_timestamp ), ''YYYY-MM-DD HH24:MI:SS'')) AS "sbc_online_appointments.max_time_time",
+        sbc_online_appointments.session_id AS "sbc_online_appointments.session_id",
+        sbc_online_appointments.appointment_step AS "sbc_online_appointments.appointment_step",
+        sbc_online_appointments.client_id AS "sbc_online_appointments.client_id",
+        sbc_online_appointments.appointment_id AS "sbc_online_appointments.appointment_id"
+    FROM looker.BM_snowplow_web_block_sbc_online_appointments AS sbc_online_appointments
+    WHERE ((( sbc_online_appointments.min_timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments.min_timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) )))))
+    GROUP BY -- decided to include GROUP BY to clean up duplicate rows, but this is not a necessary step and could be removed
+        1,
+        2,
+        3,
+        4,
+        5,
+        6
+    ORDER BY
+        1
 )
