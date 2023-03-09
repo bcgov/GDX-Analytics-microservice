d22839cdb402040fc25f38b9b8ed4db589f8bb92
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2023-03-09 14:26:18 -0800

    deleted not needed files

--- s3_to_redshift/ddl/test4256.sql
+++ s3_to_redshift/ddl/test4256.sql
@@ -1,5 +1,4 @@
 BEGIN;
--- When a SKU is not in the new csv, set a date_removed as today
 UPDATE test.gdxdsd4256
   SET date_removed = CURRENT_DATE 
   WHERE
@@ -8,7 +7,7 @@ UPDATE test.gdxdsd4256
         SELECT Site FROM test.gdxdsd4256 WHERE date_added IS NULL
     );
 
--- Update the SKU as per the new csv, set a date_removed as null if it was added previously
+
 WITH copy AS (select * from test.gdxdsd4256 where data_status = 'new')
 UPDATE test.gdxdsd4256 SET 
     asset_tag = copy.asset_tag,
@@ -20,7 +19,7 @@ UPDATE test.gdxdsd4256 SET
 FROM copy
 WHERE gdxdsd4256.site = copy.site;
 
--- set the new additions status to 'old' to prepare for the next step of removing duplicate values.
+
 UPDATE test.gdxdsd4256 SET 
     date_added = CURRENT_DATE,
     data_status = 'old'
@@ -29,7 +28,7 @@ WHERE
   SELECT Site FROM test.gdxdsd4256 WHERE data_status = 'old'
   );
 
--- Remove duplicate values as we dont need it anymore.
+
 DELETE FROM test.gdxdsd4256 
   WHERE
     data_status = 'new';
