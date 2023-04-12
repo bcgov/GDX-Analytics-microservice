ec6ef4538a329a72260ce83e0d9da3e10f63eea5
Mark Walle <Mark.Walle@gov.bc.ca>
2023-04-12 23:27:42 +0000

    Added latest_step and duration column

--- redshift_to_s3/dml/sbc_oa_final_step_of_session_last_full_day.sql
+++ redshift_to_s3/dml/sbc_oa_final_step_of_session_last_full_day.sql
@@ -1,22 +1,26 @@
 SELECT * FROM (
-    -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments?qid=XWockxwvaClhUQh0PaPrXH&origin_space=37&toggle=fil,vis
+    -- based off of the explore https://analytics.gov.bc.ca/explore/snowplow_web_block/sbc_online_appointments?qid=h7xCJDWbC91weEUQstlvJv&origin_space=37&toggle=fil,vis
     -- using the stable name PDT BM_snowplow_web_block_sbc_online_appointments
     SELECT
         (TO_CHAR(DATE_TRUNC(''second'', sbc_online_appointments.min_timestamp ), ''YYYY-MM-DD HH24:MI:SS'')) AS "sbc_online_appointments.min_time_time",
             (TO_CHAR(DATE_TRUNC(''second'', sbc_online_appointments.max_timestamp ), ''YYYY-MM-DD HH24:MI:SS'')) AS "sbc_online_appointments.max_time_time",
         sbc_online_appointments.session_id AS "sbc_online_appointments.session_id",
+        sbc_online_appointments.latest_step AS "sbc_online_appointments.latest_step",
         sbc_online_appointments.appointment_step AS "sbc_online_appointments.appointment_step",
         sbc_online_appointments.client_id AS "sbc_online_appointments.client_id",
-        sbc_online_appointments.appointment_id AS "sbc_online_appointments.appointment_id"
+        sbc_online_appointments.appointment_id AS "sbc_online_appointments.appointment_id",
+        DATEDIFF(''seconds'', sbc_online_appointments.min_timestamp,sbc_online_appointments.max_timestamp)  AS "sbc_online_appointments.duration"
     FROM looker.BM_snowplow_web_block_sbc_online_appointments AS sbc_online_appointments
     WHERE ((( sbc_online_appointments.min_timestamp  ) >= ((DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ))) AND ( sbc_online_appointments.min_timestamp  ) < ((DATEADD(day,1, DATEADD(day,-1, DATE_TRUNC(''day'',GETDATE()) ) )))))
-    GROUP BY -- decided to include GROUP BY to clean up duplicate rows, but this is not a necessary step and could be removed
+    GROUP BY
         1,
         2,
         3,
         4,
         5,
-        6
+        6,
+        7,
+        8
     ORDER BY
         1
 )
