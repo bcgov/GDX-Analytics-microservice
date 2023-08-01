67d255179a0a8800e5c37d33a62c361af5e9f946
doughon <76968796+doughon@users.noreply.github.com>
2023-08-01 13:38:08 -0700

    Escaped single quote characters

--- redshift_to_s3/dml/webdata_tran_assetsinternet_monthly.sql
+++ redshift_to_s3/dml/webdata_tran_assetsinternet_monthly.sql
@@ -14,16 +14,16 @@ SELECT * FROM (
   FROM 
     cmslite.metadata 
     LEFT JOIN microservice.asset_downloads_derived  AS asset_downloads 
-      ON SPLIT_PART(SPLIT_PART(asset_downloads.asset_url,'?', 1), '#', 1) = hr_url
-      AND (((( asset_downloads.date_timestamp  ) >= ((DATEADD(month,-1, DATE_TRUNC('month', DATE_TRUNC('day',GETDATE())) ))) 
-      AND ( asset_downloads.date_timestamp  ) < ((DATEADD(month,1, DATEADD(month,-1, DATE_TRUNC('month', DATE_TRUNC('day',GETDATE())) ) )))))) 
+      ON SPLIT_PART(SPLIT_PART(asset_downloads.asset_url,''?'', 1), ''#'', 1) = hr_url
+      AND (((( asset_downloads.date_timestamp  ) >= ((DATEADD(month,-1, DATE_TRUNC(''month'', DATE_TRUNC(''day'',GETDATE())) ))) 
+      AND ( asset_downloads.date_timestamp  ) < ((DATEADD(month,1, DATEADD(month,-1, DATE_TRUNC(''month'', DATE_TRUNC(''day'',GETDATE())) ) )))))) 
        
   WHERE
-    (hr_url LIKE 'https://www2.gov.bc.ca/assets/gov/driving-and-transportation%' 
-    OR hr_url LIKE 'https://www2.gov.bc.ca/assets/gov/transportation-infrastructure-projects%' 
-    OR hr_url LIKE 'https://www2.gov.bc.ca/assets/gov/kicking-horse-canyon-project%' 
-    OR hr_url LIKE 'https://www2.gov.bc.ca/assets/gov/government/about-the-bc-government/transportation%')
-    AND site_id = 'CA4CBBBB070F043ACF7FB35FE3FD1081'
+    (hr_url LIKE ''https://www2.gov.bc.ca/assets/gov/driving-and-transportation%'' 
+    OR hr_url LIKE ''https://www2.gov.bc.ca/assets/gov/transportation-infrastructure-projects%'' 
+    OR hr_url LIKE ''https://www2.gov.bc.ca/assets/gov/kicking-horse-canyon-project%'' 
+    OR hr_url LIKE ''https://www2.gov.bc.ca/assets/gov/government/about-the-bc-government/transportation%'')
+    AND site_id = ''CA4CBBBB070F043ACF7FB35FE3FD1081''
   GROUP BY 1,2,3,4,5,6,7,8
   ORDER BY 1,2,4 ASC
 )
