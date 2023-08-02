b9c4b76757d62a5d2da02ac110ea8b5b5c0281da
doughon <76968796+doughon@users.noreply.github.com>
2023-08-02 08:53:19 -0700

    Updated column name

--- s3_to_redshift/ddl/test.microservice_iam_key_rotation.sql
+++ s3_to_redshift/ddl/test.microservice_iam_key_rotation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS test.microservice_iam_key_rotation
 ( 
-  "date_of_test" VARCHAR(20) ENCODE ZSTD
+  "verification_phrase" VARCHAR(20) ENCODE ZSTD
 );
 
 GRANT SELECT ON test.microservice_iam_key_rotation TO "looker";
