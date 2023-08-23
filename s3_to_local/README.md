66e9403f2368d663f02767cf8acd7afe0515a3d7
Veenu Punyani <veenucan24@gmail.com>
2023-08-23 15:50:53 -0700

    s3 to local script

---
+++ s3_to_local/README.md
@@ -0,0 +1,49 @@
+# S3 File Downloader Script
+
+This script allows you to download files from an Amazon S3 bucket to a local directory, while also moving the downloaded files to a new destination within the S3 bucket.
+
+## Prerequisites
+
+- Python 3.x installed on your system.
+- An Amazon Web Services (AWS) account with access to the S3 service.
+- AWS access key ID and secret access key with appropriate permissions to read and write to the specified bucket.
+
+## Usage
+
+1. Clone or download this repository to your local machine.
+
+2. Install required Python packages using pip or pip3:
+    pip install boto3 
+    pip3 install boto3
+
+3. Create a `config.json` file in the same directory using the config_sample.json script provided and replace the placeholders with your actual values.
+
+```json
+{
+  "bucket_name": "YOUR_BUCKET_NAME",
+  "prefix": "YOUR_PREFIX",
+  "local_directory": "YOUR_LOCAL_DIRECTORY",
+  "aws_access_key_id": "YOUR_AWS_ACCESS_KEY_ID",
+  "aws_secret_access_key": "YOUR_AWS_SECRET_ACCESS_KEY",
+  "aws_default_region": "YOUR_AWS_REGION",
+  "destination_key": "YOUR_DESTINATION_KEY"
+}
+
+```
+## Run the script using the following command:
+
+python3 download_s3.py
+
+## Configuration file explained.
+
+```json
+{
+  "bucket_name": "The name of the S3 bucket you want to download files from",
+  "prefix": "The prefix of the files you want to download within the bucket",
+  "local_directory": "The local directory where downloaded files will be saved",
+  "aws_access_key_id": "Your AWS access key ID",
+  "aws_secret_access_key": "Your AWS secret access key",
+  "aws_default_region": "The AWS region of your S3 bucket",
+  "destination_key": "The destination key within the bucket where downloaded files will be moved"
+} 
+    ```
---
+++ s3_to_local/download_s3.py
@@ -0,0 +1,59 @@
+import os
+import boto3
+import json
+
+def download_s3_folder(bucket_name, prefix, local_directory, aws_access_key_id, aws_secret_access_key, aws_default_region, destination_key):
+    # Create S3 clients and resources using provided credentials and region
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
+    s3_resource = boto3.resource('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
+    
+    # Create a paginator to list objects in the S3 bucket
+    paginator = s3_client.get_paginator('list_objects_v2')
+    operation_parameters = {'Bucket': bucket_name, 'Prefix': prefix}
+
+    # Iterate through each page of objects in the bucket
+    for page in paginator.paginate(**operation_parameters):
+        if 'Contents' in page:
+            for s3_object in page['Contents']:
+                # Get the file name and construct the local file path
+                file_name = s3_object['Key']
+                local_file_path = os.path.join(local_directory, os.path.basename(file_name))
+
+                # Skip directories
+                if file_name.endswith('/'):
+                    continue
+
+                # Create the directory structure if it doesn't exist
+                os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+
+                try:
+                    # Download the file from S3 to the local file path
+                    s3_client.download_file(bucket_name, file_name, local_file_path)
+                    print(f"Downloaded {file_name} to {local_file_path}")
+
+                    # Move the file to the new destination directory in S3
+                    destination_path = os.path.join(destination_key, os.path.basename(file_name))
+                    s3_resource.Object(bucket_name, destination_path).copy_from(
+                        CopySource={'Bucket': bucket_name, 'Key': file_name}
+                    )
+                    s3_resource.Object(bucket_name, file_name).delete()
+                    print(f"Moved {file_name} to {destination_path}")
+
+                except Exception as e:
+                    print(f"Error downloading {file_name}: {e}")
+
+# Load configuration from JSON file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# Extract configuration values
+bucket_name = config['bucket_name']
+prefix = config['prefix']
+local_directory = config['local_directory']
+aws_access_key_id = config['aws_access_key_id']
+aws_secret_access_key = config['aws_secret_access_key']
+aws_default_region = config['aws_default_region']
+destination_key = config['destination_key']
+
+# Call the main function
+download_s3_folder(bucket_name, prefix, local_directory, aws_access_key_id, aws_secret_access_key, aws_default_region, destination_key)
