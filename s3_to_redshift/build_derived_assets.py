32add041b1aa4ec49bd65a76a193864dbfa00115
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-05-24 13:38:51 -0700

    asset_data_to_redshift: conf files

--- s3_to_redshift/build_derived_assets.py
+++ s3_to_redshift/build_derived_assets.py
@@ -118,7 +118,7 @@ dbname='{dbname}' host='{host}' port='{port}' user='{user}' password={password}
            password=os.environ['pgpass'])
 
 
-with open('ddl/build_derived_assets.sql', 'r') as file:
+with open('ddl/GDXDSD-6391-test.gdxdsd_6391_asset_downloads.sql', 'r') as file:
     query = file.read()
 query = query.format(schema_name=schema_name,
            asset_host=asset_host,
