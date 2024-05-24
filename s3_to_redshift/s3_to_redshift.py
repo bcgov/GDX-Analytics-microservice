abff5a913fc913dac4f452a2a8c2520a46db73eb
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-05-24 11:48:12 -0700

    s3_to_redshift: no con string

--- s3_to_redshift/s3_to_redshift.py
+++ s3_to_redshift/s3_to_redshift.py
@@ -127,13 +127,13 @@ my_bucket = resource.Bucket(bucket)  # subsitute this for your s3 bucket name.
 bucket_name = my_bucket.name
 
 # Database connection string
-conn_string = """
-dbname='{dbname}' host='{host}' port='{port}' user='{user}' password={password}
-""".format(dbname='snowplow',
-           host='redshift.analytics.gov.bc.ca',
-           port='TEST',
-           user=os.environ['pguser'],
-           password=os.environ['pgpass'])
+# conn_string = """
+# dbname='{dbname}' host='{host}' port='{port}' user='{user}' password={password}
+# """.format(dbname='snowplow',
+#            host='redshift.analytics.gov.bc.ca',
+#            port='5439',
+#            user=os.environ['pguser'],
+#            password=os.environ['pgpass'])
 
 
 def copy_query(this_table, this_batchfile, this_log):
